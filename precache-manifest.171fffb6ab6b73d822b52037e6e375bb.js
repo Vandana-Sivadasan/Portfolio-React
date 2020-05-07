c47ec1ffbc242855594360ad9a84206dbb05ac05
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-05-07 17:19:14 -0500

    Updates

---
+++ precache-manifest.171fffb6ab6b73d822b52037e6e375bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37dc10f6ee7c3d9b7faea402d339b164",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "0d4cac37d4417b6cbf9d",
+    "url": "/portfolio-react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0941ee7b6dc3e601a2a9",
+    "url": "/portfolio-react/static/css/main.72464c30.chunk.css"
+  },
+  {
+    "revision": "0d4cac37d4417b6cbf9d",
+    "url": "/portfolio-react/static/js/2.46aa4842.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio-react/static/js/2.46aa4842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0941ee7b6dc3e601a2a9",
+    "url": "/portfolio-react/static/js/main.ce845307.chunk.js"
+  },
+  {
+    "revision": "39ac857cd17bf51e1eb6",
+    "url": "/portfolio-react/static/js/runtime-main.907c719b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd0956ed48568d05b5790a5aaaeb816b",
+    "url": "/portfolio-react/static/media/picture.fd0956ed.jpg"
+  }
+]);
